a2bf79ca6bd9a159320d852ec84633cafb8962b2
Ishaan Mac <ishaannarain2022@u.northwestern.edu>
2023-07-27 13:39:10 +0800

    Updating image files

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -7,7 +7,7 @@ const Data = () => {
             </h1>
         <h3 className="home__subtitle">Aspiring Software Engineer</h3>
         <p className="home__description">
-          Aspiring software engineer studying Computer Science and Mechanical Engineering at Northwestern University. Passionate about the intersection of Math, Machine Learning, and Computer Science.
+          Aspiring software engineer studying Computer Science and Mechanical Engineering at Northwestern University. Passionate about the intersection of Math, Machine Learning, and Computer Science. Currently seeking fulltime opportunities starting Fall 2023 - Summer 2024.
           <br/><br/>
           Graduate Researcher @ NU <a href="http://www.presciencelab.org/" className="home__description-link" target="_blank">Prescience Lab</a>.
           <br/>
