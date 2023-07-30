f4f19082374835d01939b02ff60d71a44794805a
Ishaan Mac <ishaannarain2022@u.northwestern.edu>
2023-07-30 23:01:51 +0800

    Fixing page linking again

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <BrowserRouter basename='/IshaanNarain'>
-    <App />
-  // </BrowserRouter>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   // <BrowserRouter basename='/IshaanNarain'>
+//     <App />
+//   // </BrowserRouter>
+// );
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
